0ac22a51ffeab4250df3ad677e41c59f97833149
fidojahfal <fidojahfalprayoga@gmail.com>
2025-11-29 03:21:57 +0700

    feat: create initialization file for chat feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { ProfileModule } from './profile/profile.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-  imports: [CommonModule, UserModule, ProfileModule],
+  imports: [CommonModule, UserModule, ProfileModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
