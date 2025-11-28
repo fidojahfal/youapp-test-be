060ad5885ffcb72afe9dd59ff28f4102764023fd
fidojahfal <fidojahfalprayoga@gmail.com>
2025-11-29 00:36:27 +0700

    feat: create and initialize file for handling upload photo in profile

---
+++ src/common/decorator/file.decorator.ts
@@ -0,0 +1,33 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  HttpException,
+} from '@nestjs/common';
+
+export const UploadedPhotoValidated = createParamDecorator(
+  (data: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    const file = request.file;
+
+    if (!file) {
+      return null;
+    }
+
+    const validatePhoto = (file: Express.Multer.File) => {
+      if (file.size > 5 * 1024 * 1024) {
+        throw new HttpException('Maximum size of photo is 5mb!', 400);
+      }
+
+      const typeAllowed: string[] = ['image/jpeg', 'image/png', 'image/jpg'];
+      if (!typeAllowed.includes(file.mimetype)) {
+        throw new HttpException('Only image file allowed!', 400);
+      }
+
+      return file;
+    };
+
+    return {
+      photo: validatePhoto(file),
+    };
+  },
+);
--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import path from 'path';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: path.join(__dirname, '..', '..', 'picture'),
+        filename: (req, file, cb) => {
+          const filename: string = `${Date.now()}-${Math.round(Math.random() * 1e9)}-${file.originalname}`;
+          cb(null, filename);
+        },
+      }),
+    }),
+  ],
   providers: [ProfileService],
   controllers: [ProfileController],
 })
