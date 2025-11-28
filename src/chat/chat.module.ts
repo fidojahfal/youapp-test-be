3abe3f65da09010ea8fbeb3f1cc3d6bd4e70031a
fidojahfal <fidojahfalprayoga@gmail.com>
2025-11-29 03:29:22 +0700

    feat: implement rmq to chat module

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'CHAT_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: 'chat_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
+  ],
   providers: [ChatService],
   controllers: [ChatController],
 })
